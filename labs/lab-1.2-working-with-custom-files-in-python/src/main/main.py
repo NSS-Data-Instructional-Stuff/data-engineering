2cf0ff2f2e3a1ad7073cd9f3aa44b9954702ec86
daniel-wallace-personal <wallace.danielk@gmail.com>
2025-05-29 14:30:35 -0500

    fix typos and reword labs based on student feedback

---
+++ labs/lab-1.2-working-with-custom-files-in-python/src/main/main.py
@@ -0,0 +1,16 @@
+def parse_data(input_data):
+    print("Parsing data...")
+    # You will write the code here to parse data from the file
+
+def main():
+    print("Starting the main function...")
+    # Read the input file
+
+    # Call the parse_data function with the input data
+
+    # Print the parsed data to the console
+
+if __name__ == "__main__":
+    main()
+    print("Main function completed.")
+    
